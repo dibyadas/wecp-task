5935162a61436e147571e5963d7ddbc921a3192a
Dibya Prakash Das <dibyadascool@gmail.com>
2018-10-24 00:47:48 +0530

    add loading message

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -15,10 +15,12 @@ export class AdminComponent implements OnInit {
    }
 
   data = {};
-  
+  loading = true;
+  not_loading = false;
+
   ngOnInit() {
     this._fetchService.getActivity(this.id).subscribe(
-      data => { this.data = data; console.log(data); },
+      data => { this.loading = false; this.not_loading = true; this.data = data; console.log(data);  },
       err => console.error(err)
      );
   }
